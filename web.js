8a6f1b43aed41723bd76d87652d038631435f1a5
shikevin <kevin@dare2tee.com>
2013-08-13 17:54:44 +0000

    edited web.js

--- web.js
+++ web.js
@@ -1,4 +1,9 @@
+#!/usr/bin/env node
+
 var express = require('express');
+var fs = require ('fs');
+var htmlfile="index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -6,7 +11,7 @@ app.get('/', function(request, response) {
 response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
